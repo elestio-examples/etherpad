03ab2ab551d10f5cccf3a1e0e74906f31b4e1491
AmsellemJoseph <amsellem.joseph@gmail.com>
2023-09-28 13:42:13 +0300

    fix

--- scripts/preBackup.sh
+++ scripts/preBackup.sh
@@ -1,5 +1,2 @@
 #set env vars
 set -o allexport; source .env; set +o allexport;
-
-#do a consistent dump before files are copied
-docker-compose exec -T mariadb /usr/bin/mysqldump --no-tablespaces --user=root --password=$SOFTWARE_PASSWORD --all-databases | gzip > ./lastDump.sql.gz
